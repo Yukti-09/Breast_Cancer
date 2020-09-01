fb00b7d32e9cb4b4d4f7b53cf7d62ae0c1c612dd
Yukti Khosla <yuktikhosla@gmail.com>
2020-09-01 16:49:16 +0530

    Create model.py

---
+++ model.py
@@ -0,0 +1,16 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+data = datasets.load_breast_cancer()
+label_names = data['target_names']
+labels = data['target']#holds 0 for malignant tumor and 1 for benign tumor
+feature_names = data['feature_names']
+features = data['data']
+train,test,train_labels,test_labels = train_test_split(features,labels,test_size=0.2,random_state=0)
+
+classifier = GaussianNB()
+model = classifier.fit(train,train_labels)
+pred = classifier.predict(test)
+print(accuracy_score(test_labels,pred))
