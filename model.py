65410f3d39379061fb0c24ffc7df55afe3898689
Yukti Khosla <yuktikhosla@gmail.com>
2020-09-02 12:54:34 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
 data = datasets.load_breast_cancer()
@@ -10,7 +10,7 @@ feature_names = data['feature_names']
 features = data['data']
 train,test,train_labels,test_labels = train_test_split(features,labels,test_size=0.2,random_state=0)
 
-classifier = GaussianNB()
+classifier = RandomForestClassifier()
 model = classifier.fit(train,train_labels)
 pred = classifier.predict(test)
 print(accuracy_score(test_labels,pred))
